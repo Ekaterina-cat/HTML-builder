940178594e340b8fae953fbca574fe30fa3a6de4
Ekaterina <ekaterinaaleks9@gmail.com>
2024-01-21 22:08:07 +0300

    feat: add code to perform task from 04-copy-directory folders

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.mkdir(
+  path.join(__dirname, "files-copy"),
+  {recursive: true},
+    (err) => {
+      if (err) throw err;
+    }
+);
+
+fs.readdir(path.join(__dirname, "files"), 
+  (err, docs) => { 
+    if (err) throw err;
+      docs.forEach(doc => {
+        fs.copyFile(path.join(__dirname, "files", doc), path.join(__dirname, "files-copy", doc),
+        (err) => {
+          if(err) throw err;
+        });
+      });
+});
