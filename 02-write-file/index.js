8f078906cd0d3b7c717bb2717b6cc12094c93bd3
Ekaterina <ekaterinaaleks9@gmail.com>
2024-01-23 12:03:11 +0300

    fix: change the code to exit the console correctly

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,20 +1,21 @@
 const fs = require("fs");
 const path = require("path");
+const process = require('node:process');
 const { stdin, stdout } = process;
-fs.writeFile(path.join(__dirname, "version.txt"), "Create new file!",
+fs.writeFile(path.join(__dirname, "02-write-file.txt"), "Create new file!",
 (err) => {
   if(err) throw err;
   console.log("Hello, student!");
 });
 stdin.on("data", (info) => {
   const newTextEntry = info.toString();
-  fs.appendFile(path.join(__dirname, "version.txt"), `\nYou introduced: ${newTextEntry}`,
+  if (newTextEntry.trim() === "exit") {
+    process.exit();
+  }
+  fs.appendFile(path.join(__dirname, "02-write-file.txt"), `\nYou introduced: ${newTextEntry}`,
     function (error) {
       if(error) throw error;
-      if (`${newTextEntry}` === "exit") {
-        process.exit();
-      }
-    });        
+    });
 });
 
 process.on("SIGINT", () => {
